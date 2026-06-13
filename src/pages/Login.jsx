a63bcfc20131f805e9ec106ecea53fb2632fb6e5
daniliard <srmolodorchik@gmail.com>
2026-06-13 21:53:19 +0300

    login mod

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -48,9 +48,10 @@ export default function Login({ onLogin }) {
 
     const script = document.createElement('script')
     script.src = 'https://telegram.org/js/telegram-widget.js?22'
+    script.setAttribute('data-userpic', 'false')
+    script.setAttribute('data-auth-url', window.location.origin)
     script.setAttribute('data-telegram-login', 'Wishlle_bot')
     script.setAttribute('data-size', 'large')
-    script.setAttribute('data-onauth', 'onTelegramAuth(user)')
     script.setAttribute('data-request-access', 'write')
     script.async = true
 
